20b158b62f182785e4ae55251d9864ada1121fc8
Patrick Power <patrickpower@Patricks-Mac-mini.local>
2020-12-01 10:00:00 +0000

    Updated Bootstrap version

--- functions.php
+++ functions.php
@@ -19,14 +19,14 @@ function excerpt($limit, $id) {
 
 function enqueues() //Enqueue the CSS & jQuery files...
 	{
-		wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' ); // BOOTSTRAP CSS
+		wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css' ); // BOOTSTRAP CSS
 		//wp_enqueue_style( 'slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css');		// Slick CSS
 		wp_enqueue_style( 'style', get_stylesheet_uri(),'','0.36','' );		// STYLE.CSS
     
 		wp_register_script('fontawesome','https://use.fontawesome.com/releases/v5.1.0/js/all.js','','');// FontAwesome
 		wp_register_script('scripts', get_template_directory_uri(). '/js/scripts.js',array('jquery'),'',true); // THE TEMPLATE SCRIPTS
 		//wp_register_script('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js',array('jquery'),'',true); // Slick JS (Carousel)
-		wp_enqueue_script( 'bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',array('jquery'),'',true ); // BOOTSTRAP JS
+		wp_enqueue_script( 'bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js',array('jquery'),'',true ); // BOOTSTRAP JS
 	
 		wp_deregister_script('jquery'); // THIS LINE DEREGISTERS WP'S STANDARD JQUERY...
 		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', false, '1.3.2');// ...THIS LINE REPLACES THE ABOVE WITH GOOGLE'S JQUERY...
